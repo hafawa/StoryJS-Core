8eaffd384ce570671d547f243aed4caf32ed579b
hafawa <r.hafawa@gmail.com>
2014-09-30 09:55:28 +0100

    Update en.js

--- Language/locale/en.js
+++ Language/locale/en.js
@@ -2,9 +2,9 @@
 ================================================== */
 if(typeof VMM != 'undefined') {
 	VMM.Language = {
-		lang: "en",
+		lang: "ar",
 		api: {
-			wikipedia: "en"
+			wikipedia: "ar"
 		},
 		date: {
 			month: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
